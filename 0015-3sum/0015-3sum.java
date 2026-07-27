c119a8e954f161f9dc1cab8486c0a9ed78c3c35b
Sailesh Kumar <saileshkl63072@gmail.com>
2026-07-27 16:05:12 +0530

    Time: 27 ms (98.91%), Space: 59.3 MB (45.73%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,44 +4,40 @@ class Solution {
         int j = nums.length-1;
 
         while(i<j){
-            int sum = nums[f] + nums[i] + nums[j];
+            int sum = nums[f]+nums[i]+nums[j];
             if(sum > 0){
                 j--;
 
             }else if(sum < 0){
                 i++;
-
             }else{
-                res.add(Arrays.asList(nums[f], nums[i], nums[j]));
-                i = i + 1;
-                j = j - 1;
+                res.add(Arrays.asList(nums[f],nums[i],nums[j]));
+                i++;
+                j--;
 
-                while(i<j && nums[i] == nums[i-1]){
+                while(i < j && nums[i] == nums[i-1]){
                     i++;
-                }while(i<j && nums[j] == nums[j+1]){
+                }while(i < j && nums[j] == nums[j+1]){
                     j--;
                 }
-
-                  }
+            }
         }
-
     }
+ 
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
 
-List<List<Integer>> res = new ArrayList<>();
         for(int f = 0; f<nums.length; f++){
-            if(nums[f] > 0){
+            if (nums[f] > 0){
                 break;
-
-
             }
-            if(f ==0 || nums[f] != nums[f-1]){
-    twoSumHelper(f , nums , res);
-}
+            if(f == 0|| nums[f]!= nums[f-1]){
+            twoSumHelper(f,nums,res);
         }
-           return res;
         }
         
-    
+        return res;
+     
+}
 }
