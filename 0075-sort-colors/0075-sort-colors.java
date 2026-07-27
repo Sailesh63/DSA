3ad1263fe16e1e717812e76b5d03655339ed5f3c
Sailesh Kumar <saileshkl63072@gmail.com>
2026-07-27 17:50:33 +0530

    Time: 0 ms (100%), Space: 43.5 MB (61.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,24 +3,23 @@ class Solution {
         int i = 0;
         int j = nums.length-1;
         int k = 0;
-       
-       while(k<=j){
-        if(nums[k] == 1){
-            k = k + 1;
-        }else if(nums[k] == 2){
-            int temp = nums[j];
-            nums[j] = nums[k];
-            nums[k] = temp;
-            j--;
 
-        }else{
-            int temp = nums[i];
-            nums[i] = nums[k];
-            nums[k] = temp;
-
-            k = k + 1;
-            i = i + 1;
+        while(k<=j){
+            if(nums[k] == 1){
+                k++;
+            }else if(nums[k] == 2){
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                j--;
+            }else{
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
+                i++;
+                k++;
+            }
         }
-       }
+
     }
 }
