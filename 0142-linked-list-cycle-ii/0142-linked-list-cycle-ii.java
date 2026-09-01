e661e1d936caf376d0fe6db2f13db00751193cee
Sailesh Kumar <saileshkl63072@gmail.com>
2026-09-01 22:58:17 +0530

    Time: 0 ms (100%), Space: 46.7 MB (30.53%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -18,24 +18,23 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
 
-            if( slow == fast){
+            if(slow == fast){
                 break;
-
             }
+
         }
         if(fast == null || fast.next == null){
             return null;
         }
-
         ListNode n1 = slow;
         ListNode n2 = head;
 
-        while( n1 != n2){
+        while(n1 != n2){
             n1 = n1.next;
             n2 = n2.next;
         }
         return n1;
-
+       
        
     }
 }
